14ac7885e9f0f8ad1201bf8ce70de9fad814a126
Vinnyramos <vinnyramossilva@gmail.com>
2017-04-13 14:57:08 -0300

    esse é o primeiro commit do projeto restaurante

---
+++ src/main/java/com/project/restaurante/JpRefeiçãoApplication.java
@@ -0,0 +1,12 @@
+package com.project.restaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpRefeiçãoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpRefeiçãoApplication.class, args);
+	}
+}
